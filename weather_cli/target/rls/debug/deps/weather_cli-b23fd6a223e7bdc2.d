65e383ed77730dd859f37f249a5933a82995716b
ajg7 <ajgebara@gmail.com>
2021-02-10 10:42:19 -0600

    got rid of extra folder

--- weather_cli/target/rls/debug/deps/weather_cli-b23fd6a223e7bdc2.d
+++
@@ -1,5 +0,0 @@
-c:\Users\ajgeb\Desktop\My_Projects\weather_cli\Weather_CLI\weather_cli\target\rls\debug\deps\weather_cli-b23fd6a223e7bdc2.rmeta: src\main.rs
-
-c:\Users\ajgeb\Desktop\My_Projects\weather_cli\Weather_CLI\weather_cli\target\rls\debug\deps\weather_cli-b23fd6a223e7bdc2.d: src\main.rs
-
-src\main.rs:
