2dd2fe08fa5ddb941c04a1b50cd29ca1049be0b3
ajg7 <ajgebara@gmail.com>
2021-02-09 23:18:35 -0600

    initialized a rust project

---
+++ weather_cli/target/rls/debug/deps/weather_cli-609a71aaef3863eb.d
@@ -0,0 +1,5 @@
+c:\Users\ajgeb\Desktop\My_Projects\weather_cli\Weather_CLI\weather_cli\target\rls\debug\deps\weather_cli-609a71aaef3863eb.rmeta: src\main.rs
+
+c:\Users\ajgeb\Desktop\My_Projects\weather_cli\Weather_CLI\weather_cli\target\rls\debug\deps\weather_cli-609a71aaef3863eb.d: src\main.rs
+
+src\main.rs:
